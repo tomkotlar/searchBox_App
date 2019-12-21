1cdcdcffbdd216431334b5fcb94323fcb37df8a8
tomkotlar <t.kotlar@yahoo.com>
2019-12-21 19:57:51 +0000

    Updates

---
+++ precache-manifest.aa464369aec12746b5b96c0542c094ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b4332036ed6f5d131a3d39c5c68cf74",
+    "url": "/new-employees/index.html"
+  },
+  {
+    "revision": "d4337b6cb46e84e4d1dc",
+    "url": "/new-employees/static/css/main.1fdd07f2.chunk.css"
+  },
+  {
+    "revision": "2dd225d375a641707331",
+    "url": "/new-employees/static/js/2.94e63ccf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/new-employees/static/js/2.94e63ccf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d4337b6cb46e84e4d1dc",
+    "url": "/new-employees/static/js/main.de2f668d.chunk.js"
+  },
+  {
+    "revision": "1edeac0fbd5760f4af98",
+    "url": "/new-employees/static/js/runtime-main.091b96fd.js"
+  }
+]);
